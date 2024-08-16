a979db71e518e88f0199dc6c2291b6a72ef71e47
sharmaboy7210 <sharmamohit7210@gmail.com>
2024-08-16 13:09:26 +0530

    buttons function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let btn = document.querySelector(".copy-button")
-let connectbtn = document.querySelector(".connect")
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let btn3 = document.querySelector(".btn3")
+
 
 let copy = 0 
 
@@ -19,11 +22,24 @@ btn.addEventListener("click", function(){
 });
 
 
+let connectbtn = document.querySelector(".connect")
+
 connectbtn.addEventListener("click", ()=>{
     alert("✨Connect with Mohit Sharma Now!✨ \n \nTap OK to begin an exciting conversation and explore new possibilities! 🌟👋 ")
     window.location.href ="https://www.linkedin.com/in/sharmaboy7210/"
 })
 
 
+btn1.addEventListener("click", function(){
+    window.location.href = "https://www.instagram.com/sharmaboy7210/reels/"
+})
+
+btn2.addEventListener("click", function(){
+    window.location.href = "https://www.youtube.com/watch?v=vxd50n6kxM8&list=PL-oqR2CR6Rca9wJGisREn68DuI6p_8vL9&pp=iAQB"
+})
+
+btn1.addEventListener("click", function(){
+    window.location.href = "https://www.instagram.com/reel/C-FDSH3xgmk/"
+})
 
 
